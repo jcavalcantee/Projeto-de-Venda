ba8a238cc289dc4d84a3f10d81b9ac55d880975e
jcavalcantee <jcavalcantessilva@gmail.com>
2023-09-22 21:40:05 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/prototipos/Prototipos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prototipos;
+
+/**
+ *
+ * @author jeffe
+ */
+public class Prototipos {
+
+    public static void main(String[] args) {
+        
+     
+        
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+}
